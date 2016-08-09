ca2b44209bcca602759a017df0d67048fad42d21
Thomas Langnickel <Langnickel.thomas@gmail.com>
2016-08-09 22:43:51 +1000

    refactored to all commands within command file

--- lib/commands.rb
+++ lib/commands.rb
@@ -3,14 +3,20 @@ require_relative 'table'
 
 class Commands
 	VALID_COMMANDS = ["PLACE X,Y,FACING", "MOVE", "LEFT", "RIGHT", "REPORT", "SELFDESTRUCT", "EXIT"]
+	TURNING = {"LEFT" => -1, "RIGHT" => 1}
 
+	#grabing variables from robot and the table
 	def initialize
 		@table = Table.new
 		@robot = Robot.new(@table)
 		@height = @table.instance_variable_get(:@height)
 		@width = @table.instance_variable_get(:@width)
 		@min = @table.instance_variable_get(:@min)
+		@x = @robot.instance_variable_get(:@x)
+		@y = @robot.instance_variable_get(:@y)
+		@robot_direction = @robot.instance_variable_get(:@robot_direction)
 	end
+
 	#introduction
 	def greeting
 		puts "Welcome commander."
@@ -26,19 +32,22 @@ class Commands
 		puts "6) #{VALID_COMMANDS[5]} .... will destroy the robot and exit the program"
 		puts "7) #{VALID_COMMANDS[6]} .... will exit the program"
 	end
- #validates command from command array
+
+	#validates command from command array
 	def valid_command?(command)
 		VALID_COMMANDS.include?(command)
 	end
+
  # after executed command as for new command
 	def input_command
 		loop do
-			# puts "Please enter #{VALID_COMMANDS.join(' or ')}"
 			puts ""
 			puts "What are your commands my master?"
 			give_command(gets.chomp.upcase)
 		end
 	end
+
+
  # what happens at which command
 	def give_command(command)
 		if command.start_with? "PLACE"
@@ -59,16 +68,21 @@ class Commands
 		else
 			case command
 			when "SELFDESTRUCT" then destruct
-			when "MOVE" then @robot.move
-			when "REPORT" then @robot.report
-			when "LEFT" then @robot.rotate("LEFT")
-			when "RIGHT" then @robot.rotate("RIGHT")
+			when "MOVE" then move
+			when "REPORT" then report
+			when "LEFT" then rotate("LEFT")
+			when "RIGHT" then rotate("RIGHT")
 			#rotate to allow for more directions (e.g. other angles than 90 degrees)
 			end
 		end
 	end
 
-	#extract position and direction with validation
+	#shorten if-condition, for checking of correct placement and feeding more conditions later if required
+	def valid_placement?
+		@table.placed_on_table?(@x, @y) && @robot.vectors.include?(@robot_direction)
+	end
+
+	#extract position and direction with validation from place command
 	def place(command)
 		command.sub!("PLACE", "")
 		if (command =~ /\s*\d,\d,\w+/).nil?
@@ -79,7 +93,7 @@ class Commands
   		@x = number_or_nil(@x)
   		@y = number_or_nil(@y)
     end
-
+		#validates the location of the placement
 		if valid_placement?
 			@robot.location(@x, @y, @robot_direction)
 			puts ""
@@ -88,17 +102,43 @@ class Commands
 			puts "..the place command is incorrectly formatted or outside the table bounds. "
 			puts "The boundries of this world reach from #{@min},#{@min} up to #{@width - 1},#{@height - 1}"
 			puts "A place command looks like \'PLACE 0,0,North \' "
-			# puts "The tables boundries are #{table_range(@height)} x #{table_range(@width)}"
 		end
-
 	end
 
-	#shorten if condition, for checking of corect placement
-	def valid_placement?
-		@table.placed_on_table?(@x, @y) && @robot.vectors.include?(@robot_direction)
+	#checks if  "MOVE" command keeps robot on table
+	def valid_move?
+		@table.stays_on_table?(@robot_direction, @x, @y)
 	end
 
-	#fun
+	#change of coodinates by the "MOVE" command
+	def move
+		if valid_move?
+			puts ""
+			puts "Roger, roger I\'m moving forward one field!"
+			case @robot_direction
+			when "EAST" then @x += 1
+			when "WEST" then @x -= 1
+			when "NORTH" then @y += 1
+			when "SOUTH" then @y -= 1
+			end
+		else
+      puts "This is the end of this world and I can't go further in this direction, please change direction"
+		end
+	end
+
+	#allows to iterate through the directions not effected by amount of directions, therefore scalable and easy to add more directions
+	def rotate(direction)
+		new_vector = (@robot.vectors[@robot_direction] + TURNING[direction]) % @robot.vectors.length
+		@robot_direction = @robot.vectors.key(new_vector)
+		puts "Roger, roger. I\'ve turned to the #{direction}! I\'m now facing #{@robot_direction}"
+	end
+
+	#reporting by the "REPORT" command
+	def report
+		puts "My current location is (#{@x}, #{@y}) facing #{@robot_direction}."
+	end
+
+	#destroying existing robot and enable creation of new robot
 	def destruct
 		puts ""
 		puts "...whyyyyyyyyyy!!!"
@@ -111,7 +151,6 @@ class Commands
 		puts "Your robot exploded... The world is save (until you place a new robot)"
 		@robot.remove
 	end
-
 
 	# Prevents against a non-numberic input in the place command. a.to_i => 0 which could hide an error. Thus instead nil is used.
 	def number_or_nil(string)
--- lib/robot.rb
+++ lib/robot.rb
@@ -1,7 +1,6 @@
 require_relative 'directions'
 
 class Robot
-	TURNING = {"LEFT" => -1, "RIGHT" => 1}
 
 	def initialize(table)
 		@table = table
@@ -17,9 +16,10 @@ class Robot
 	def location(x, y, robot_direction)
 		@x = x
 		@y = y
-		@facing = robot_direction
+		@robot_direction = robot_direction
 	end
 
+	#removes instance variable x so robot_placed? returns false and allows to place a new robot
 	def remove
 		remove_instance_variable(:@x)
 	end
@@ -28,39 +28,6 @@ class Robot
 	#if if robot has been placed
 	def robot_placed?
 		!!@x
-	end
-
-	#allows to iterate through the directions not effected by amount of directions
-	def rotate(direction)
-		new_vector = (vectors[@facing] + TURNING[direction]) % vectors.length
-		@facing = vectors.key(new_vector)
-		puts "Roger, roger. I\'ve turned to the #{direction}! I\'m now facing #{@facing}"
-	end
-
-	#change of coodinates by the "MOVE" command
-	def move
-		if valid_move?
-			puts ""
-			puts "Roger, roger I\'m moving forward one field!"
-			case @facing
-			when "EAST" then @x += 1
-			when "WEST" then @x -= 1
-			when "NORTH" then @y += 1
-			when "SOUTH" then @y -= 1
-			end
-		else
-      puts "This is the end of this world and I can't go further in this direction, please change direction"
-		end
-	end
-
-	#reporting by the "REPORT" command
-	def report
-		puts "My current location is (#{@x}, #{@y}) facing #{@facing}."
-	end
-
-	#checks if  "MOVE" command keeps robot on table
-	def valid_move?
-		@table.stays_on_table?(@facing, @x, @y)
 	end
 
 end
